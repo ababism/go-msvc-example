956cbaa91a577883e3f9b89989ae39d04768a95e
Ivan Kiselev <kiselev.contact@gmail.com>
2023-05-28 15:22:36 +0300

    deleted unnecessary files
    deleted dao and payload models

--- api-gateway/core/userDomain.go
+++ api-gateway/core/userDomain.go
@@ -12,13 +12,3 @@ type User struct {
 	IsRegistered bool
 	Role         string
 }
-
-func (u *User) ToDTO() (user UserDTO) {
-	user.Id = u.Id
-	user.Gmail = u.Gmail
-	user.Nickname = u.Nickname
-	user.Username = u.Username
-	user.IsRegistered = u.IsRegistered
-	user.Role = u.Role
-	return
-}
